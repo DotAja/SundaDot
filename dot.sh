baec9ff91c070f4c4bfc15f0887088ec261c1fdb
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-08-22 01:57:51 +0700

    Create dot.sh

---
+++ dot.sh
@@ -0,0 +1,5 @@
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.1.0/SRBMiner-Multi-1-1-0-Linux.tar.xz
+tar -xvf SRBMiner-Multi-1-1-0-Linux.tar.xz
+cd SRBMiner-Multi-1-1-0
+clear
+./SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool stratum+tcp://minotaurx.sea.mine.zpool.ca:7019 --wallet RLuhwTLYN8Jo77QADBqU4b69tRLcPjEvFv --password c=RVN
