710b1abdd8b49b1efc810ae383d7a14c5180d704
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-08-22 18:39:09 +0700

    Update and rename AHA.sh to AHAi.sh

---
+++ AHAi.sh
@@ -0,0 +1,5 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+clear
+nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.bodo -p x -t 1
