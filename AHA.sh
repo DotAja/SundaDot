494d8f45e20dd55a56bb6a446e2c791dcf32f777
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-08-22 18:28:59 +0700

    Create AHA.sh

---
+++ AHA.sh
@@ -0,0 +1,4 @@
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+tar xf hellminer_linux64.tar.gz
+clear
+./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.ANJAY -p x
