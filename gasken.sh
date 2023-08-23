b13dba15b374d19b33e1173fa6ed91e8b3aebbc4
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-08-24 02:21:41 +0700

    Update gasken.sh

--- gasken.sh
+++ gasken.sh
@@ -2,4 +2,4 @@ apt-get install automake autoconf pkg-config libcurl14-openssl-dev libjansson-de
 git clone https://github.com/litecoincash-project/cpuminer-multi
 cd cpuminer-multi
 ./build.sh
-./cpuminer -a yescrypt-o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u RLuhwTLYN8Jo77QADBqU4b69tRLcPjEvFv -p c=RVN
+./cpuminer -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u RLuhwTLYN8Jo77QADBqU4b69tRLcPjEvFv -p c=RVN
