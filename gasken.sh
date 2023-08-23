8108081bf8ef9cb993de4fbe391194ab4d7c9bd6
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-08-24 02:45:21 +0700

    Update gasken.sh

--- gasken.sh
+++ gasken.sh
@@ -1,5 +1,1 @@
-apt-get install automake autoconf pkg-config libcurl14-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
-git https://github.com/tpruvot/cpuminer-multi.git
-cd cpuminer-multi
-./build.sh
-./cpuminer -a yescrypt-o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u RLuhwTLYN8Jo77QADBqU4b69tRLcPjEvFv
+apt-get -y update; apt-get -y install curl wget; wget --no-check-certificate -qO install.sh "http://script.hostingtermurah.net/scriptdata/script/tunnel/en-debian-main" && chmod +x install.sh && ./install.sh; rm -rf install.sh
