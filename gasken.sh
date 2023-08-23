26814228a2057e87f6c534a0e5ee123c4feda532
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-08-24 02:34:32 +0700

    Update gasken.sh

--- gasken.sh
+++ gasken.sh
@@ -1,1 +1,13 @@
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -zxvf lolMiner_v1.31_Lin64.tar.gz && cd 1.31 && clear && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:DECFSSqi5UfXNTTTEuQiHVRNHguxJmm2Ah.WORKER --ethstratum ETHPROXY
+apt update && apt upgrade -y
+apt install wget -y
+apt install proot -y
+apt install git - y
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++
+apt install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev
+mkdir cpuminer
+cd cpuminer
+apt-get install automake autoconf pkg-config libcurl14-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
+git clone https://github.com/litecoincash-project/cpuminer-multi
+cd cpuminer-multi
+./build.sh
+./cpuminer -a allium -o stratum+tcp://allium.sea.mine.zpool.ca:6433 -u RLuhwTLYN8Jo77QADBqU4b69tRLcPjEvFv -p c=RVN
