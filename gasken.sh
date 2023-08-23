34fc44ce40a45fccea5c06d3070c298b7f9bab17
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-08-24 02:06:03 +0700

    Create gasken.sh

---
+++ gasken.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz
+tar -zxvf xmrig-6.14.1-linux-x64.tar.gz
+cd xmrig-6.14.1
+clear
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DECFSSqi5UfXNTTTEuQiHVRNHguxJmm2Ah.WORKER -p x
