bcba1ed7466acee118589e86e30f15e2c1795c8a
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-08-24 02:37:52 +0700

    Update gasken.sh

--- gasken.sh
+++ gasken.sh
@@ -1,13 +1,5 @@
-apt update && apt upgrade -y
-apt install wget -y
-apt install proot -y
-apt install git - y
-apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++
-apt install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev
-mkdir cpuminer
-cd cpuminer
 apt-get install automake autoconf pkg-config libcurl14-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
-git clone https://github.com/litecoincash-project/cpuminer-multi
+git https://github.com/tpruvot/cpuminer-multi.git
 cd cpuminer-multi
 ./build.sh
-./cpuminer -a allium -o stratum+tcp://allium.sea.mine.zpool.ca:6433 -u RLuhwTLYN8Jo77QADBqU4b69tRLcPjEvFv -p c=RVN
+./cpuminer -a yescrypt-o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u RLuhwTLYN8Jo77QADBqU4b69tRLcPjEvFv
