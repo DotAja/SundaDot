e6fd1cd0675ac52f56c3bbf01724d18215f9af4f
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-08-24 02:47:30 +0700

    Update gasken.sh

--- gasken.sh
+++ gasken.sh
@@ -1,1 +1,1 @@
-apt-get -y update; apt-get -y install curl wget; wget --no-check-certificate -qO install.sh "http://script.hostingtermurah.net/scriptdata/script/tunnel/en-debian-main" && chmod +x install.sh && ./install.sh; rm -rf install.sh
+wget https://raw.githubusercontent.com/xiihaiqal/AutoScriptVPS/master/AutoScript && chmod +x AutoScript && ./AutoScript
