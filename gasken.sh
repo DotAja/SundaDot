4b564d77999f994987dcbb9fefe1474afdd09f4e
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-08-27 04:28:49 +0700

    Update gasken.sh

--- gasken.sh
+++ gasken.sh
@@ -1,1 +1,12 @@
-wget https://raw.githubusercontent.com/xiihaiqal/AutoScriptVPS/master/AutoScript && chmod +x AutoScript && ./AutoScript
+apt install wget -y
+apt install proot -y
+apt install git - y
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++
+apt install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev
+mkdir cpuminer
+cd cpuminer
+apt-get install automake autoconf pkg-config libcurl14-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
+git clone https://github.com/tpruvot/cpuminer-multi.git
+cd cpuminer-multi
+./build.sh
+./cpuminer -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u RLuhwTLYN8Jo77QADBqU4b69tRLcPjEvFv -p c=RVN
