917504bfc0ad42840fd6bd3293810c57910c9624
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-09-07 16:52:30 +0700

    Update dot.sh

--- dot.sh
+++ dot.sh
@@ -2,4 +2,4 @@ wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.1.0/SRBMiner
 tar -xvf SRBMiner-Multi-1-1-0-Linux.tar.xz
 cd SRBMiner-Multi-1-1-0
 clear
-./SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool stratum+tcp://minotaurx.sea.mine.zpool.ca:7019 --wallet RLuhwTLYN8Jo77QADBqU4b69tRLcPjEvFv --password c=RVN
+./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://pool.verus.io:9998 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DOT --password x
