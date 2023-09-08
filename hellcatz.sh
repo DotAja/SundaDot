e00eacf6935c420aecdb9397a225c6bd0da769ae
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-09-08 15:13:22 +0700

    Create hellcatz.sh

---
+++ hellcatz.sh
@@ -0,0 +1,4 @@
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+tar xf hellminer_linux64.tar.gz
+clear
+./hellminer -c stratum+tcp://pool.verus.io:9998#xnsub -u RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.MinerDOT -p x
