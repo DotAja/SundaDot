9e9ae9f520c20481a6596d856e92da1d66b15f2d
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-09-11 19:09:07 +0700

    Update dot.sh

--- dot.sh
+++ dot.sh
@@ -2,4 +2,4 @@ wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner
 tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz
 cd SRBMiner-Multi-2-3-5
 clear
-./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://pool.verus.io:9998 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DOT --password x -t 3
+./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://pool.verus.io:9998 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DOT --password x
