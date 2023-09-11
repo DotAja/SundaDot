092b0321cde9a2926667364403fcceb4e4b2d567
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-09-11 17:01:30 +0700

    Update AHAi.sh

--- AHAi.sh
+++ AHAi.sh
@@ -2,4 +2,4 @@ wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-L
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
 clear
-nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.bodo -p x -t 1
+nheqminer/nheqminer -v -l pool.verus.io:9998 -u RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.bodo -p x
