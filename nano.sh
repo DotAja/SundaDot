70df8c4dbb5085b80f977434052c73ab1d46871b
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-09-13 18:15:31 +0700

    Update nano.sh

--- nano.sh
+++ nano.sh
@@ -1,1 +1,4 @@
-
+wget https://github.com/nanopool/nanominer/releases/download/v3.8.5/nanominer-linux-3.8.5.tar.gz
+tar xf nanominer-linux-3.8.5.tar.gz
+clear
+./nanominer -algo verushash -wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p -rigName DOTnano -pool1 eu.luckpool.net:3956 -coin VRSC
