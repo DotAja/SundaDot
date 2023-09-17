4b589dd011199805c382b384821ac06475db4a48
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-09-18 03:25:24 +0700

    Update dotminer.sh

--- dotminer.sh
+++ dotminer.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 clear
 echo "JANGAN LUPA BAHAGIA YA GAES!!!"
 echo "#####      #####    ########
@@ -15,11 +16,15 @@ if [ $pil = "luck" ]||[ $pil = "LUCK" ]
     tar xf nanominer-linux-3.8.5.tar.gz
     clear
     ./nanominer -algo verushash -wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p -rigName DOTminer -pool1 eu.luckpool.net:3956 -coin VRSC
-    done
   elif [ $pil = "pool" ]||[ $pil = "POOL" ]
   then
     wget https://github.com/nanopool/nanominer/releases/download/v3.8.5/nanominer-linux-3.8.5.tar.gz
     tar xf nanominer-linux-3.8.5.tar.gz
     clear
     ./nanominer -algo verushash -wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p -rigName DOTminer -pool1 pool.verus.io:9998 -coin VRSC
-    done
+  else
+     echo "\033[1;31mERROr : Wrong Input....!"
+     sleep 2
+  fi
+done
+done
