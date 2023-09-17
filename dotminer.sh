d98ce5a21c59ea2fdd772af05ecc5363d7e0352c
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-09-18 03:07:49 +0700

    Create dotminer.sh

---
+++ dotminer.sh
@@ -0,0 +1,25 @@
+clear
+echo "JANGAN LUPA BAHAGIA YA GAES!!!"
+echo "#####      #####    ########
+echo "##   ##  ##     ##     ##
+echo "##   ##  ##     ##     ##    ####    ####    ####
+echo "##   ##  ##     ##     ##   ##  ##      ##  ##  ##
+echo "##   ##  ##     ##     ##   ######     ##   ######
+echo "#####      #####       ##   ##  ##   ##     ##  ##
+echo "            \033[1;32m  BY HENDI KUSNANDI\n\n"
+echo -n "KETIKAN YANG INGIN DI MINER luck/pool : "
+read pil;
+if [ $pil = "luck" ]||[ $pil = "LUCK" ]
+  then
+    wget https://github.com/nanopool/nanominer/releases/download/v3.8.5/nanominer-linux-3.8.5.tar.gz
+    tar xf nanominer-linux-3.8.5.tar.gz
+    clear
+    ./nanominer -algo verushash -wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p -rigName DOTminer -pool1 eu.luckpool.net:3956 -coin VRSC
+    done
+  elif [ $pil = "pool" ]||[ $pil = "POOL" ]
+  then
+    wget https://github.com/nanopool/nanominer/releases/download/v3.8.5/nanominer-linux-3.8.5.tar.gz
+    tar xf nanominer-linux-3.8.5.tar.gz
+    clear
+    ./nanominer -algo verushash -wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p -rigName DOTminer -pool1 pool.verus.io:9998 -coin VRSC
+    done
