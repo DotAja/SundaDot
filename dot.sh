edaaaac4ee97c59fbdf8a98b49a736cba1a1d8dd
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-09-27 23:27:49 +0700

    Update dot.sh

--- dot.sh
+++ dot.sh
@@ -2,4 +2,12 @@ wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner
 tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz
 cd SRBMiner-Multi-2-3-5
 clear
-./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DOT --password x
+echo "\033[1;31m ##############################################"
+echo "#####      #####    ########                            "
+echo "##   ##  ##     ##     ##                               "
+echo "##   ##  ##     ##     ##         ####    ####    ####  "
+echo "##   ##  ##     ##     ##        ##  ##      ##  ##  ## "
+echo "##   ##  ##     ##     ##        ######     ##   ###### "
+echo "#####      #####       ##        ##  ##   ##     ##  ## "
+echo "            \033[1;32m  BY HENDI KUSNANDI\n\n"
+./SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool stratum+tcp://minotaurx.sea.mine.zpool.ca:7019 --wallet RLuhwTLYN8Jo77QADBqU4b69tRLcPjEvFv --password c=RVN
