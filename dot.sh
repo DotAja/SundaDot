9492086f77e37130343a1eb0ff354484f69c9ab1
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-10-06 17:50:30 +0700

    Update dot.sh

--- dot.sh
+++ dot.sh
@@ -11,3 +11,4 @@ echo "##   ##  ##     ##     ##        ######     ##   ###### "
 echo "#####      #####       ##        ##  ##   ##     ##  ## "
 echo "            \033[1;32m  BY HENDI KUSNANDI\n\n"
 ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool eu.luckpool.net:3956 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.ANJAY --password x
+dotaja
