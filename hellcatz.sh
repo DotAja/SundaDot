d57f1e0823343d7152c8c6bef03ec6027bfafb1d
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-10-06 18:17:59 +0700

    Update hellcatz.sh

--- hellcatz.sh
+++ hellcatz.sh
@@ -1,3 +1,4 @@
-git clone https://github.com/monkins1010/ccminer.git
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
 cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
 ./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DOTaja -p x
