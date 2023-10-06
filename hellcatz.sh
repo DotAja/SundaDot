0dadb71542cfde720b28df845084664301ca65d6
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-10-06 18:08:39 +0700

    Update hellcatz.sh

--- hellcatz.sh
+++ hellcatz.sh
@@ -1,5 +1,3 @@
-wget https://github.com/nanopool/nanominer/releases/download/v3.8.5/nanominer-linux-3.8.5.tar.gz
-tar xf nanominer-linux-3.8.5.tar.gz
-clear
-./nanominer -algo verushash -wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p -rigName DOTaja -pool1 eu.luckpool.net:3956 -coin VRSC
-dotaja
+git clone –single-branch -b ARM https://github.com/monkins1010/ccminer.git
+cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
+./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DOTaja -p x
