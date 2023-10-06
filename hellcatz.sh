409dac41fafa0aa01f67e34c440d9f0e30ea8a9a
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-10-06 18:15:09 +0700

    Update hellcatz.sh

--- hellcatz.sh
+++ hellcatz.sh
@@ -1,3 +1,3 @@
-git clone –single-branch -b ARM https://github.com/monkins1010/ccminer.git
+git clone https://github.com/monkins1010/ccminer.git
 cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
 ./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DOTaja -p x
