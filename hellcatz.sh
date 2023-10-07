6dedb208fab719ff4f2d33997953a9371d8cd89c
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-10-07 18:44:34 +0700

    Update hellcatz.sh

--- hellcatz.sh
+++ hellcatz.sh
@@ -1,4 +1,5 @@
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
-cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
-./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DOTaja -p x
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner-Multi-2-3-5-Linux.tar.xz
+tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz
+cd SRBMiner-Multi-2-3-5
+clear
+./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DOTsolo --password d=16,hybrid
