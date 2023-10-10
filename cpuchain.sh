c4c069aaf93b48352ab26939ed8b19ba7891b215
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-10-10 16:17:25 +0700

    Update cpuchain.sh

--- cpuchain.sh
+++ cpuchain.sh
@@ -1,4 +1,4 @@
 wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/v1.0/Cpuminer-opt-cpupower-linux64.tar.gz
 tar zxvf Cpuminer-opt-cpupower-linux64.tar.gz
 clear
-./cpuminer -a cpupower -o stratum+tcp://cpupower.eu.mine.zpool.ca:6240 -u RPSHCyYazKW1BVkiqupjTLEacvyoTX67ud.DOT -p c=CPU
+./cpuminer -a cpupower -o stratum+tcp://cpupower.eu.mine.zpool.ca:6240 -u RPSHCyYazKW1BVkiqupjTLEacvyoTX67ud.DOT -p c=RVN
