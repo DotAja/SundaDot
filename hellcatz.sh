87bdf74f924637097348942c3855fa7a33e149f8
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-10-13 03:50:11 +0700

    Update hellcatz.sh

--- hellcatz.sh
+++ hellcatz.sh
@@ -1,5 +1,5 @@
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner-Multi-2-3-5-Linux.tar.xz
-tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz
-cd SRBMiner-Multi-2-3-5
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz
+tar -zxvf xmrig-6.14.1-linux-x64.tar.gz
+cd xmrig-6.14.1
 clear
-./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DOTsolo --password d=16,hybrid
+./xmrig -o zephyr.miningocean.org:5332 -u ZEPHsCdTeo19PQCTciqJ4qBYyFhjfsMCCVRWZZdYYZ1ZT2zUpX52c64BJZpwed7MU3YRA2Q2zTN29XN2udKJDHYvYfdDANV14uq -p DOT -a rx/0
