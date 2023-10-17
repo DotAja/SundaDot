f00a88bc720b07b7e4a052c4a83f26a4c9e64a62
kilyotir <142836390+kilyotir@users.noreply.github.com>
2023-10-17 14:44:24 +0700

    Update hellcatz.sh

--- hellcatz.sh
+++ hellcatz.sh
@@ -1,5 +1,14 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz
-tar -zxvf xmrig-6.14.1-linux-x64.tar.gz
-cd xmrig-6.14.1
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner-Multi-2-3-5-Linux.tar.xz
+tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz
+cd SRBMiner-Multi-2-3-5
 clear
-./xmrig -o zephyr.miningocean.org:5332 -u ZEPHsCdTeo19PQCTciqJ4qBYyFhjfsMCCVRWZZdYYZ1ZT2zUpX52c64BJZpwed7MU3YRA2Q2zTN29XN2udKJDHYvYfdDANV14uq -p DOT -a rx/0
+echo "\033[1;31m ##############################################"
+echo "#####      #####    ########                            "
+echo "##   ##  ##     ##     ##                               "
+echo "##   ##  ##     ##     ##         ####    ####    ####  "
+echo "##   ##  ##     ##     ##        ##  ##      ##  ##  ## "
+echo "##   ##  ##     ##     ##        ######     ##   ###### "
+echo "#####      #####       ##        ##  ##   ##     ##  ## "
+echo "            \033[1;32m  BY HENDI KUSNANDI\n\n"
+./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://ap.vipor.net:5040 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p --password x
+dotaja
