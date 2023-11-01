2c494a25a3e55e77fcbef3c35d8d5049219239ac
DotAja <142836390+DotAja@users.noreply.github.com>
2023-11-02 02:42:11 +0700

    Create dotsrb.sh

---
+++ dotsrb.sh
@@ -0,0 +1,15 @@
+sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner-Multi-2-3-5-Linux.tar.xz
+12345678
+tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz
+cd SRBMiner-Multi-2-3-5
+clear
+echo "\033[1;31m ##############################################"
+echo "#####      #####    ########                            "
+echo "##   ##  ##     ##     ##                               "
+echo "##   ##  ##     ##     ##         ####    ####    ####  "
+echo "##   ##  ##     ##     ##        ##  ##      ##  ##  ## "
+echo "##   ##  ##     ##     ##        ######     ##   ###### "
+echo "#####      #####       ##        ##  ##   ##     ##  ## "
+echo "            \033[1;32m  BY HENDI KUSNANDI\n\n"
+sudo ./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DotAja --password x
+dotaja
