ee7cf5252bc6fa3580e226129e4c4b53eb8f2909
DotAja <142836390+DotAja@users.noreply.github.com>
2024-01-22 19:36:28 +0700

    Update dotsrb.sh

--- dotsrb.sh
+++ dotsrb.sh
@@ -1,5 +1,4 @@
 sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner-Multi-2-3-5-Linux.tar.xz
-12345678
 tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz
 cd SRBMiner-Multi-2-3-5
 clear
@@ -11,5 +10,4 @@ echo "##   ##  ##     ##     ##        ##  ##      ##  ##  ## "
 echo "##   ##  ##     ##     ##        ######     ##   ###### "
 echo "#####      #####       ##        ##  ##   ##     ##  ## "
 echo "            \033[1;32m  BY HENDI KUSNANDI\n\n"
-sudo ./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DotAja --password x -t 14
-dotaja
+./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DotAja --password x
