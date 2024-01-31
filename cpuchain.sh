0f08eac20ba87453d89ae5dc6b1ad2fedd1b1cc4
DotAja <142836390+DotAja@users.noreply.github.com>
2024-02-01 03:15:20 +0700

    Update cpuchain.sh

--- cpuchain.sh
+++ cpuchain.sh
@@ -1,4 +1,7 @@
-wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/v1.0/Cpuminer-opt-cpupower-linux64.tar.gz
-tar zxvf Cpuminer-opt-cpupower-linux64.tar.gz
+mkdir baru
+cd baru
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz
+tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz
 clear
-./cpuminer -a cpupower -o stratum+tcp://pool.crionic.org:5665 -u CepsVZPdDWc43utnKX9XAe7V8AAXq3mRih.DotAja -p c=CPU
+nohup ./cpuminer -a Yespower -o stratum+tcp://na.mining4people.com:3338 -u uj1SsbrKhFAzUwsu6GL3VP8FTh35f8d26z.AkuBot -t 4
+tetek
