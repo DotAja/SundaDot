f3e4eeda8a9295cdeb874a9ab76412c26a9065ed
DotAja <142836390+DotAja@users.noreply.github.com>
2024-03-22 00:05:11 +0700

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Perbarui repositori paket
+sudo apt update
+
+# Instal Docker dan npm
+sudo apt install -y docker.io npm
+
+# Kloning repositori stratum-ethproxy dari GitHub
+git clone https://github.com/oneevil/stratum-ethproxy
+
+# Masuk ke direktori stratum-ethproxy
+cd stratum-ethproxy
+
+# Instal dependensi npm dari proyek stratum-ethproxy
+npm install
+
+# Mendapatkan alamat IP lokal
+LOCAL_IP=$(hostname -I | awk '{print $1}')
+
+# Menambahkan nilai ke dalam file .env
+echo "REMOTE_HOST=51.81.230.49" >> .env
+echo "REMOTE_PORT=20020" >> .env
+echo "REMOTE_PASSWORD=x" >> .env
+echo "LOCAL_HOST=$LOCAL_IP" >> .env
+echo "LOCAL_PORT=443" >> .env
+
+# Mulai stratum-ethproxy menggunakan npm start dalam sesi screen
+sudo screen -dmS stratumeth npm start
+
+# Tampilkan sesi screen yang telah dibuat
+sudo screen -r
