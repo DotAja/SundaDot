0dbe4469b5d2c5a4fe619ff206212b0554972c5b
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-02 18:47:19 +0700

    Update dotminer.sh

--- dotminer.sh
+++ dotminer.sh
@@ -2,4 +2,4 @@ wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.7/SRBMiner
 tar -xvf SRBMiner-Multi-2-4-7-Linux.tar.xz
 cd SRBMiner-Multi-2-4-7
 clear
-./SRBMiner-MULTI  --algorithm aurum --pool stratum-asia.rplant.xyz:7114 --wallet waf1qs7djrfr2kj4w8qkylva5tr24500l3nael0l34g.DOT --keepalive true
+./SRBMiner-MULTI --algorithm aurum --pool stratum-asia.rplant.xyz:7114 --wallet waf1qs7djrfr2kj4w8qkylva5tr24500l3nael0l34g.DOT --keepalive true
